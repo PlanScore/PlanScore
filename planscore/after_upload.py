43c6823d2a4d5a3c28903a4ac202dcb304a6f91f
Michal Migurski <mike@teczno.com>
2017-05-16 13:01:29 -0700

    Testing Lambda proxy integration

--- planscore/after_upload.py
+++ planscore/after_upload.py
@@ -9,6 +9,12 @@ def lambda_handler(event, context):
     pprint.pprint(dir(context))
     pprint.pprint(context.client_context)
     pprint.pprint(dir(context.client_context))
+    
+    return {
+        'statusCode': '200',
+        'headers': {'X-Custom-Header': 'Yo'},
+        'body': 'Hello world'
+        }
 
 if __name__ == '__main__':
     pass
