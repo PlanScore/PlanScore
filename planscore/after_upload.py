4fab31e0600f791aa130bb41998af53ac6cd705f
Michal Migurski <mike@teczno.com>
2017-05-17 11:44:50 -0700

    Starting to read actual data from S3 uploads

--- planscore/after_upload.py
+++ planscore/after_upload.py
@@ -1,10 +1,26 @@
-import boto3, pprint
+import boto3, pprint, os, io, tempfile, shutil, contextlib
 from . import util
+
+@contextlib.contextmanager
+def temporary_buffer_file(filename, buffer):
+    try:
+        dirname = tempfile.mkdtemp(prefix='temporary_buffer_file-')
+        filepath = os.path.join(dirname, filename)
+        with open(filepath, 'wb') as file:
+            file.write(buffer.read())
+        yield filepath
+    finally:
+        shutil.rmtree(dirname)
 
 def get_uploaded_info(s3, bucket, key):
     '''
     '''
     upload = s3.get_object(Bucket=bucket, Key=key)
+    
+    with temporary_buffer_file(os.path.basename(key), upload['Body']) as name:
+        with open(name, 'rb') as file:
+            pass # print(file.name, repr(file.read()))
+    
     return '{ContentLength} bytes in {0}'.format(key, **upload)
 
 def lambda_handler(event, context):
--- planscore/tests/test_after_upload.py
+++ planscore/tests/test_after_upload.py
@@ -1,10 +1,23 @@
-import unittest, unittest.mock, io
+import unittest, unittest.mock, io, os, contextlib
 from .. import after_upload
 
 class TestAfterUpload (unittest.TestCase):
     
     def test_get_uploaded_info(self):
+        '''
+        '''
+        nullplan_path = os.path.join(os.path.dirname(__file__), 'data', 'null-plan.geojson')
+
+        @contextlib.contextmanager
+        def nullplan_file(*args):
+            yield nullplan_path
+        
         s3 = unittest.mock.Mock()
-        s3.get_object.return_value = {'ContentLength': 99}
-        info = after_upload.get_uploaded_info(s3, 'planscore', 'uploads/null.txt')
-        self.assertEqual(info, '99 bytes in uploads/null.txt')
+        s3.get_object.return_value = {'ContentLength': 1119, 'Body': None}
+
+        with unittest.mock.patch('planscore.after_upload.temporary_buffer_file') as temporary_buffer_file:
+            temporary_buffer_file.side_effect = nullplan_file
+            info = after_upload.get_uploaded_info(s3, 'planscore', 'uploads/null-plan.geojson')
+
+        temporary_buffer_file.assert_called_once_with('null-plan.geojson', None)
+        self.assertEqual(info, '1119 bytes in uploads/null-plan.geojson')
