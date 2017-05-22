d29c0d886208ca414bf9ed38a9af4a3d8d268832
Michal Migurski <mike@teczno.com>
2017-05-22 09:18:39 -0700

    Added map display from geometry GeoJSON

--- planscore/website/static/plan.js
+++ planscore/website/static/plan.js
@@ -34,7 +34,7 @@ function load_plan_score(url, fields, table, eff_gap)
         {
             // Returns a dictionary with a list of districts
             var data = JSON.parse(request.responseText);
-            console.log('loaded', data);
+            console.log('Loaded plan:', data);
             
             var rows = document.getElementsByTagName('tr'),
                 row = rows[rows.length - 1],
@@ -59,6 +59,48 @@ function load_plan_score(url, fields, table, eff_gap)
             
             // Note the efficiency gap
             eff_gap.innerText = nice_gap(data['summary']['Efficiency Gap']);
+        }
+    };
+
+    request.onerror = function() { /* There was a connection error of some sort */ };
+    request.send();
+}
+
+function load_plan_map(url, div)
+{
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+
+    function on_loaded_geojson(geojson)
+    {
+        console.log('GeoJSON bounds:', geojson.getBounds());
+
+        // Initialize the map on the passed div in the middle of the ocean
+        var map = L.map(div, {
+            scrollWheelZoom: false,
+            center: [0, 0],
+            zoom: 8
+        });
+    
+        // Add Toner tiles for base map
+        L.tileLayer('http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}@2x.png', {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://maps.stamen.com/">Map tiles</a> by <a href="http://stamen.com/">Stamen</a>',
+            maxZoom: 18
+        }).addTo(map);
+        
+        // Add a GeoJSON layer and fit it into view
+        geojson.addTo(map);
+        map.fitBounds(geojson.getBounds());
+    }
+
+    request.onload = function()
+    {
+        if(request.status >= 200 && request.status < 400)
+        {
+            // Returns a dictionary with a list of districts
+            var data = JSON.parse(request.responseText);
+            console.log('Loaded map:', data);
+            on_loaded_geojson(L.geoJSON(data));
         }
     };
 
