1f33626f418cc48866fa2d57ac05b4490d4adc33
Michal Migurski <mike@teczno.com>
2017-05-25 08:59:18 -0700

    Wrapped districts process in stubby Lambda handler

--- planscore/districts.py
+++ planscore/districts.py
@@ -1,3 +1,24 @@
+import collections
+from osgeo import ogr
+from . import prepare_state
+
+ogr.UseExceptions()
+
+def lambda_handler(event, context):
+    '''
+    '''
+    if 'totals' in event and 'precincts' in event and 'tiles' in event:
+        totals, precincts, tiles = event['totals'], event['precincts'], event['tiles']
+    elif 'geometry' in event:
+        geometry = ogr.CreateGeometryFromWkt(event['geometry'])
+        totals, precincts, tiles = collections.defaultdict(int), [], \
+            get_geometry_tile_zxys(geometry)
+    else:
+        raise RuntimeError('Missing required totals, precincts, tiles, and geometry')
+    
+    for _ in consume_tiles(totals, precincts, tiles):
+        print('Iteration:', json.dumps(dict(totals=totals, precincts=precincts, tiles=tiles)))
+
 def consume_tiles(totals, precincts, tiles):
     ''' Generate a stream of steps, updating totals from precincts and tiles.
     
@@ -24,7 +45,7 @@ def iterate_precincts(precincts, tiles):
     '''
     while precincts or tiles:
         if precincts:
-            # There is a precincts to yield.
+            # There is a precinct to yield.
             precinct = precincts.pop(0)
             yield precinct
     
@@ -32,3 +53,22 @@ def iterate_precincts(precincts, tiles):
             # All out of precincts; fill up from the next tile.
             more_precincts = load_tile_precincts(tiles.pop(0))
             precincts.extend(more_precincts)
+
+def get_geometry_tile_zxys(district_geom):
+    ''' Return a list of expected tile Z/X/Y strings.
+    '''
+    if district_geom.GetSpatialReference():
+        district_geom.TransformTo(prepare_state.EPSG4326)
+    
+    xxyy_extent = district_geom.GetEnvelope()
+    iter = prepare_state.iter_extent_tiles(xxyy_extent, prepare_state.TILE_ZOOM)
+    tiles = []
+
+    for (coord, tile_wkt) in iter:
+        tile_zxy = '{zoom}/{column}/{row}'.format(**coord.__dict__)
+        tile_geom = ogr.CreateGeometryFromWkt(tile_wkt)
+        
+        if tile_geom.Intersects(district_geom):
+            tiles.append(tile_zxy)
+    
+    return tiles
--- planscore/tests/test_districts.py
+++ planscore/tests/test_districts.py
@@ -1,7 +1,24 @@
-import unittest
+import unittest, os, json
+from osgeo import ogr
 from .. import districts
 
 class TestDistricts (unittest.TestCase):
+
+    @unittest.mock.patch('planscore.districts.consume_tiles')
+    def test_lambda_handler_init(self, consume_tiles):
+        ''' Lambda event data with just geometry starts the process.
+        '''
+        event = {'geometry': 'POLYGON ((-0.0002360 0.0004532,-0.0006812 0.0002467,-0.0006356 -0.0003486,-0.0000268 -0.0004693,-0.0000187 -0.0000214,-0.0002360 0.0004532))'}
+        districts.lambda_handler(event, None)
+        self.assertEqual(consume_tiles.mock_calls[0][1], ({}, [], ['10/511/511', '10/511/512']))
+
+    @unittest.mock.patch('planscore.districts.consume_tiles')
+    def test_lambda_handler_continue(self, consume_tiles):
+        ''' Lambda event data with existing totals continues the process.
+        '''
+        event = {'totals': {}, 'precincts': [{'Totals': 1}], 'tiles': ['10/511/512']}
+        districts.lambda_handler(event, None)
+        self.assertEqual(consume_tiles.mock_calls[0][1], ({}, [{'Totals': 1}], ['10/511/512']))
 
     @unittest.mock.patch('planscore.districts.score_precinct')
     def test_consume_tiles(self, score_precinct):
@@ -30,6 +47,40 @@ class TestDistricts (unittest.TestCase):
         
         self.assertEqual(len(score_precinct.mock_calls), expected_calls)
     
+    @unittest.mock.patch('planscore.districts.score_precinct')
+    def test_consume_tiles_detail(self, score_precinct):
+        ''' Expected updates are made to totals dictionary and lists.
+        '''
+        def mock_score_precinct(totals, precinct):
+            totals['Voters'] += precinct['Voters']
+        
+        score_precinct.side_effect = mock_score_precinct
+
+        totals, precincts, tiles = {'Voters': 0}, [], \
+            [({'Voters': 1}, {'Voters': 2}), ({'Voters': 4}, {'Voters': 8})]
+        
+        call = districts.consume_tiles(totals, precincts, tiles)
+        self.assertEqual((totals, precincts, tiles), ({'Voters': 0}, [],
+            [({'Voters': 1}, {'Voters': 2}), ({'Voters': 4}, {'Voters': 8})]))
+        
+        next(call)
+        self.assertEqual((totals, precincts, tiles), ({'Voters': 1},
+            [{'Voters': 2}], [({'Voters': 4}, {'Voters': 8})]))
+        
+        next(call)
+        self.assertEqual((totals, precincts, tiles), ({'Voters': 3}, [],
+            [({'Voters': 4}, {'Voters': 8})]))
+        
+        next(call)
+        self.assertEqual((totals, precincts, tiles), ({'Voters': 7},
+            [{'Voters': 8}], []))
+        
+        next(call)
+        self.assertEqual((totals, precincts, tiles), ({'Voters': 15}, [], []))
+
+        with self.assertRaises(StopIteration):
+            next(call)
+    
     @unittest.mock.patch('planscore.districts.load_tile_precincts')
     def test_iterate_precincts(self, load_tile_precincts):
         ''' Expected list of precincts comes back from a pair of lists.
@@ -55,3 +106,20 @@ class TestDistricts (unittest.TestCase):
             self.assertEqual(actual, expected)
         
         self.assertEqual(len(load_tile_precincts.mock_calls), expected_calls)
+
+    def test_get_geometry_tile_zxys(self):
+        ''' Get an expected list of Z/X/Y tile strings for a geometry.
+        '''
+        with open(os.path.join(os.path.dirname(__file__), 'data', 'null-plan.geojson')) as file:
+            geojson = json.load(file)
+        
+        feature1, feature2 = geojson['features']
+
+        geometry1 = ogr.CreateGeometryFromJson(json.dumps(feature1['geometry']))
+        done1 = districts.get_geometry_tile_zxys(geometry1)
+
+        geometry2 = ogr.CreateGeometryFromJson(json.dumps(feature2['geometry']))
+        done2 = districts.get_geometry_tile_zxys(geometry2)
+        
+        self.assertEqual(done1, ['10/511/511', '10/511/512'])
+        self.assertEqual(done2, ['10/511/511', '10/512/511', '10/511/512', '10/512/512'])
