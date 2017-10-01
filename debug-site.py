7c5e3d62dff04a2ebc355af41a5fc5e37cfca3d6
Michal Migurski <mike@teczno.com>
2017-10-01 13:56:36 -0700

    Fixed template reloading behavior in debug mode

--- debug-site.py
+++ debug-site.py
@@ -2,4 +2,6 @@
 import planscore.website
 
 if __name__ == '__main__':
+    planscore.website.app.jinja_env.auto_reload = True
+    planscore.website.app.config['TEMPLATES_AUTO_RELOAD'] = True
     planscore.website.app.run(debug=True)
