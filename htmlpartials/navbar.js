1aeeff4cad55bf730f6291890124bac57a3cd8d5
tsinn <timothysinnott@hotmail.com>
2017-12-21 09:31:18 -0500

    Navbar update
    new logo, container instead of container-fluid #73

--- htmlpartials/navbar.js
+++ htmlpartials/navbar.js
@@ -1,6 +1,6 @@
 const htmlblock = `
     <nav class="navbar navbar-default">
-        <div class="container-fluid">
+        <div class="container">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
                     <span class="sr-only">Toggle navigation</span>
@@ -8,7 +8,7 @@ const htmlblock = `
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="../home/">LOGO PlanScore</a>
+                <a class="navbar-brand" href="../images/logo_300.png">LOGO PlanScore</a>
             </div>
             <div class="collapse navbar-collapse" id="navbar">
                 <ul class="nav navbar-nav navbar-right">
