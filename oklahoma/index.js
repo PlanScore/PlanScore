fdb6105c48d56da029825b736780e86ca0b8bd20
gregallensworth <gregor@greeninfo.org>
2017-12-21 09:23:36 -0800

    #72 state page basic layout

--- oklahoma/index.js
+++ oklahoma/index.js
@@ -91,6 +91,10 @@ __webpack_require__(90);
 // CONSTANTS
 //
 
+// the currently-visible state: boundary type + year, e.g. US House districts for 1984
+// these are affected by window.selectXXX() family of functions which ultimately are all wrappers over loadDataForSelectedBoundaryAndYear()
+var CURRENT_VIEW = {};
+
 // the list of years to offer; used by the year picker so the user may choose dates
 // note that not every state has data at all levels for every year
 var PLAN_YEARS = [1972, 1974, 1976, 1978, 1980, 1982, 1984, 1986, 1988, 1990, 1992, 1994, 1996, 1998, 2000, 2002, 2004, 2006, 2008, 2010, 2012, 2014, 2016];
@@ -108,6 +112,9 @@ var MAP_CHOROPLETH_BREAKS = [{ from: -999999, to: -100, color: '#FFFFFF', title:
 //
 
 $(document).ready(function () {
+    initYearPickers();
+    initBoundaryPicker();
+    initLoadStartingConditions(); // this will implicitly call loadDataForSelectedBoundaryAndYear() after setup, loading the map
 
     $(window).on('resize', handleResize);
     handleResize();
@@ -115,6 +122,95 @@ $(document).ready(function () {
 
 window.handleResize = function () {
     // various things that don't gracefully handle being resized, so we need to help them out
+};
+
+window.initYearPickers = function () {
+    // a simple SELECT to choose a year, and trigger selectYear()
+
+    var $picker_small = $('#yearpicker-small');
+    PLAN_YEARS.slice().reverse().forEach(function (year) {
+        $('<option></option>').text(year).prop('value', year).appendTo($picker_small);
+    });
+    $picker_small.change(function () {
+        var year = $(this).val();
+        selectYear(year);
+    });
+};
+
+window.initBoundaryPicker = function () {
+    $('#boundarypicker div').click(function () {
+        var boundarytype = $(this).attr('data-boundary');
+        selectBoundaryType(boundarytype);
+    });
+};
+
+window.initLoadStartingConditions = function () {
+    // analyze the #year-polytype hash to see what year + type we should load
+    // provide some defaults
+    var year = PLAN_YEARS[PLAN_YEARS.length - 1];
+    var type = 'ushouse';
+
+    var year_and_type = /^#(\d\d\d\d)\-(\w+)$/.exec(window.location.hash);
+    if (year_and_type) {
+        year = year_and_type[1];
+        type = year_and_type[2];
+    }
+
+    // ready, set, go
+    selectYear(year);
+    selectBoundaryType(type);
+};
+
+//
+// RUNTIME FUNCTIONS FOR CHANGING YEAR + DISTRICT TYPE
+// loadDataForSelectedBoundaryAndYear() is the real worker here; the others are basically convenience functions
+//
+
+window.loadDataForSelectedBoundaryAndYear = function () {
+    if (!CURRENT_VIEW.year || !CURRENT_VIEW.boundtype) return; // need both; during startup one will be blank, so avoid an error
+    console.log(CURRENT_VIEW);
+
+    // fetch the CSV file and then use the callbacks to update the map
+    var url = '../data/bias_' + CURRENT_VIEW.boundtype + '.csv';
+    Papa.parse(url, {
+        download: true,
+        header: true,
+        complete: function complete(results) {
+            // filter by the year (geography is implicit by which CSV was fetched)
+            // and for each row assign the bias score to the state's row in the above
+            // this is loop-within-loop as we match arrays to arrays, but P=50 and Q=20ish so it's affordable
+            results = results.data.forEach(function (row) {
+                //gda filter by state and year
+            });
+
+            //gda do something
+
+            //gda do something
+        }
+    });
+
+    // update URL params to show the current search
+    // see also initLoadStartingConditions() which will load such a state
+    var hash = '#' + CURRENT_VIEW.year + '-' + CURRENT_VIEW.boundtype;
+    window.location.replace(hash);
+};
+
+window.selectYear = function (year) {
+    // UI update: highlight this button
+    $('#yearpicker-small').val(year);
+
+    // save to the state and refresh the map + data
+    CURRENT_VIEW.year = year;
+    loadDataForSelectedBoundaryAndYear();
+};
+
+window.selectBoundaryType = function (boundtype) {
+    // UI update: highlight this button
+    $('#boundarypicker div').removeClass('active').filter('[data-boundary="' + boundtype + '"]').addClass('active');
+
+    // save to the state and refresh the map + data
+    CURRENT_VIEW.boundtype = boundtype;
+    loadDataForSelectedBoundaryAndYear();
 };
 
 /***/ }),
