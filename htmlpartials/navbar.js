410cfeb2a3c43660408b7a71152736066ef904ee
tsinn <timothysinnott@hotmail.com>
2017-12-21 10:38:23 -0500

    Navbar updates
    Added SVG logo, set to responsive size for mobile #73

--- htmlpartials/navbar.js
+++ htmlpartials/navbar.js
@@ -1,6 +1,6 @@
 const htmlblock = `
     <nav class="navbar navbar-default">
-        <div class="container">
+        <div class="container-fluid">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
                     <span class="sr-only">Toggle navigation</span>
@@ -8,7 +8,7 @@ const htmlblock = `
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="../images/logo_300.png">LOGO PlanScore</a>
+                <a class="navbar-brand" href="../home/"><img id="brand-logo" src="../images/logo.svg"></img></a>
             </div>
             <div class="collapse navbar-collapse" id="navbar">
                 <ul class="nav navbar-nav navbar-right">
