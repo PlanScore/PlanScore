f5d9e305004af1a4e55c6712a5371209b8f8ca4d
Michal Migurski <mike@teczno.com>
2017-12-30 11:46:07 -0800

    Pruned unused score_plan() and score_district() from planscore.score

--- planscore/score.py
+++ planscore/score.py
@@ -6,7 +6,7 @@ of district plan and uploads summary JSON file.
 import io, os, gzip, posixpath, json, statistics, copy, time, itertools
 from osgeo import ogr
 import boto3, botocore.exceptions
-from . import prepare_state, util, data, constants
+from . import data, constants
 
 ogr.UseExceptions()
 
@@ -39,103 +39,6 @@ FIELD_NAMES += tuple([f'REP{sim:03d}' for sim in range(1000)])
 FIELD_NAMES += tuple([f'DEM{sim:03d}' for sim in range(1000)])
 
 FUNCTION_NAME = 'PlanScore-ScoreDistrictPlan'
-
-def score_plan(s3, bucket, input_upload, plan_path, tiles_prefix):
-    '''
-    '''
-    new_districts = []
-    feature_count, output = 0, io.StringIO()
-    ds = ogr.Open(plan_path)
-    print(ds, file=output)
-    
-    if not ds:
-        raise RuntimeError('Could not open file')
-    
-    for (index, feature) in enumerate(ds.GetLayer(0)):
-        feature_count += 1
-        print(index, feature, file=output)
-
-        totals, tiles, district_output = score_district(s3, bucket, feature.GetGeometryRef(), tiles_prefix)
-        output.write(district_output)
-        new_districts.append(dict(totals=totals, tiles=tiles))
-    
-    output_upload = calculate_gap(input_upload.clone(districts=new_districts))
-    length = os.stat(plan_path).st_size
-    
-    print('{} features in {}-byte {}'.format(feature_count,
-        length, os.path.basename(plan_path)), file=output) 
-    
-    print('Uploading to s3://{}/{}...'.format(bucket, output_upload.index_key()),
-        file=output)
-    
-    return output_upload, output.getvalue()
-
-def score_district(s3, bucket, district_geom, tiles_prefix):
-    '''
-    '''
-    tile_list, output = [], io.StringIO()
-    totals = {field: 0 for field in FIELD_NAMES}
-    
-    if district_geom.GetSpatialReference():
-        district_geom.TransformTo(prepare_state.EPSG4326)
-    
-    xxyy_extent = district_geom.GetEnvelope()
-    tiles = prepare_state.iter_extent_tiles(xxyy_extent, prepare_state.TILE_ZOOM)
-
-    for (coord, tile_wkt) in tiles:
-        tile_zxy = '{zoom}/{column}/{row}'.format(**coord.__dict__)
-        tile_geom = ogr.CreateGeometryFromWkt(tile_wkt)
-        
-        if not tile_geom.Intersects(district_geom):
-            continue
-        
-        try:
-            object = s3.get_object(Bucket='planscore',
-                Key='{}/{}.geojson'.format(tiles_prefix, tile_zxy))
-        except botocore.exceptions.ClientError as error:
-            if error.response['Error']['Code'] == 'NoSuchKey':
-                continue
-            raise
-
-        if object.get('ContentEncoding') == 'gzip':
-            object['Body'] = io.BytesIO(gzip.decompress(object['Body'].read()))
-        
-        with util.temporary_buffer_file('tile.geojson', object['Body']) as path:
-            ds = ogr.Open(path)
-            defn = ds.GetLayer(0).GetLayerDefn()
-            fields = [defn.GetFieldDefn(i).name for i in range(defn.GetFieldCount())]
-            for feature in ds.GetLayer(0):
-                precinct_geom = feature.GetGeometryRef()
-                
-                if not precinct_geom.Intersects(district_geom):
-                    continue
-                
-                try:
-                    overlap_geom = precinct_geom.Intersection(district_geom)
-                except RuntimeError as e:
-                    if 'TopologyException' in str(e) and not precinct_geom.IsValid():
-                        # Sometimes, a precinct geometry can be invalid
-                        # so inflate it by a tiny amount to smooth out problems
-                        precinct_geom = precinct_geom.Buffer(0.0000001)
-                        overlap_geom = precinct_geom.Intersection(district_geom)
-                    else:
-                        raise
-                overlap_area = overlap_geom.Area() / precinct_geom.Area()
-                precinct_fraction = overlap_area * feature.GetField(prepare_state.FRACTION_FIELD)
-                
-                for name in FIELD_NAMES:
-                    if name not in fields:
-                        continue
-                    precinct_value = precinct_fraction * feature.GetField(name)
-                    totals[name] += precinct_value
-                
-        tile_list.append(tile_zxy)
-        print(' ', prepare_state.KEY_FORMAT.format(directory='XX',
-            zxy=tile_zxy), file=output)
-    
-    print('>', totals, file=output)
-    
-    return totals, tile_list, output.getvalue()
 
 def calculate_EG(red_districts, blue_districts, vote_swing=0):
     ''' Convert two lists of district vote counts into an EG score.
