764829a5e2e204893f774f4f337f729bd1bb8c37
gregallensworth <gregor@greeninfo.org>
2018-01-08 12:57:25 -0800

    #140 IE polyfill Object.entries()

--- htmlpartials/navbar.js
+++ htmlpartials/navbar.js
@@ -8,7 +8,7 @@ const htmlblock = `
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/index-new.html"><img id="brand-logo" src="/images/logo.svg"></img></a>
+                <a class="navbar-brand" href="/index-new.html"><img id="brand-logo" src="/images/logo.svg" /></a>
             </div>
             <div class="collapse navbar-collapse" id="navbar">
                 <ul class="nav navbar-nav navbar-right">
