008dfa0408542d7f9468a3274dbde0efe19bd689
Michal Migurski <mike@teczno.com>
2018-02-18 16:01:36 -0800

    Removed AWS token (for now)

--- planscore/upload_fields.py
+++ planscore/upload_fields.py
@@ -28,8 +28,8 @@ def get_upload_fields(s3, creds, request_url, secret):
     
     presigned['fields'].update(acl=acl, success_action_redirect=redirect_url)
     
-    if creds.token:
-        presigned['fields']['x-amz-security-token'] = creds.token
+    #if creds.token:
+    #    presigned['fields']['x-amz-security-token'] = creds.token
     
     return presigned['url'], presigned['fields']
 
