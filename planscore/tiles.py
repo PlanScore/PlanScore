6e86e71eb784e916f1fa322b3899e0b560749ce3
Michal Migurski <mike@teczno.com>
2018-02-24 13:05:19 -0800

    Copied district.score_precinct() to tiles.score_precinct()

--- planscore/tiles.py
+++ planscore/tiles.py
@@ -1,6 +1,6 @@
 import json, io, gzip, posixpath, functools
 import osgeo.ogr, boto3, botocore.exceptions, ModestMaps.OpenStreetMap, ModestMaps.Core
-from . import constants, data, util
+from . import constants, data, util, prepare_state, score
 
 FUNCTION_NAME = 'PlanScore-RunTile'
 
@@ -42,6 +42,63 @@ def tile_geometry(tile_zxy):
         N=NW.lat, W=NW.lon, S=SE.lat, E=SE.lon)
 
     return osgeo.ogr.CreateGeometryFromWkt(wkt)
+
+def score_precinct(partial_district_geom, precinct_feat, tile_geom):
+    ''' Return weighted totals for a precinct features within a tile geometry.
+        
+        partial_district_geom is the intersection of district and tile geometries.
+    '''
+    # Initialize totals to zero
+    totals = {name: 0 for name in score.FIELD_NAMES if name in precinct_feat['properties']}
+    precinct_geom = osgeo.ogr.CreateGeometryFromJson(json.dumps(precinct_feat['geometry']))
+    
+    if precinct_geom is None or precinct_geom.IsEmpty():
+        # If there's no geometry here, don't bother.
+        return totals
+    elif precinct_geom.GetGeometryType() in (osgeo.ogr.wkbPoint,
+        osgeo.ogr.wkbPoint25D, osgeo.ogr.wkbMultiPoint, osgeo.ogr.wkbMultiPoint25D):
+        # Points have no area
+        precinct_is_point = True
+        precinct_frac = 1
+    else:
+        precinct_is_point = False
+        precinct_frac = precinct_feat['properties'][prepare_state.FRACTION_FIELD]
+
+    if precinct_frac == 0:
+        # If there's no overlap here, don't bother.
+        return totals
+
+    if tile_geom.Within(partial_district_geom):
+        # Don't laboriously calculate precinct fraction if we know it's all there.
+        # This is safe because precincts are clipped on tile boundaries, so a
+        # fully-contained tile necessarily means the precinct is also contained.
+        precinct_fraction = precinct_frac
+    elif precinct_is_point:
+        # Do simple inside/outside check for points
+        precinct_fraction = precinct_frac if precinct_geom.Within(partial_district_geom) else 0
+    else:
+        try:
+            overlap_geom = precinct_geom.Intersection(partial_district_geom)
+        except RuntimeError as e:
+            if 'TopologyException' in str(e) and not precinct_geom.IsValid():
+                # Sometimes, a precinct geometry can be invalid
+                # so inflate it by a tiny amount to smooth out problems
+                precinct_geom = precinct_geom.Buffer(0.0000001)
+                overlap_geom = precinct_geom.Intersection(partial.geometry)
+            else:
+                raise
+        if precinct_geom.Area() == 0:
+            # If we're about to divide by zero, don't bother.
+            return
+
+        overlap_area = overlap_geom.Area() / precinct_geom.Area()
+        precinct_fraction = overlap_area * precinct_frac
+    
+    for name in totals:
+        precinct_value = precinct_fraction * (precinct_feat['properties'][name] or 0)
+        totals[name] = round(precinct_value, constants.ROUND_COUNT)
+    
+    return totals
 
 def lambda_handler(event, context):
     '''
