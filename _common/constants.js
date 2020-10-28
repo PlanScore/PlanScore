99e11b57b3c25c2952c2833999126963aabd0540
danrademacher <dan@greeninfo.org>
2020-10-27 21:19:53 -0700

    Add 2018 to constants for home page animation

--- _common/constants.js
+++ _common/constants.js
@@ -9,7 +9,7 @@ export const PLAN_YEARS = [
     1980, 1982, 1984, 1986, 1988,
     1990, 1992, 1994, 1996, 1998,
     2000, 2002, 2004, 2006, 2008,
-    2010, 2012, 2014, 2016,
+    2010, 2012, 2014, 2016, 2018
 ];
 
 // the color gradient from Republican red to Democrat blue
