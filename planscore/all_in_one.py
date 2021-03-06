64efed42591b40cc41a9f5e6471bc30bc5650dc2
Michal Migurski <mike@teczno.com>
2021-03-06 13:50:44 -0800

    Switched to correct redirect from PlanScore-AllInOne

--- planscore/all_in_one.py
+++ planscore/all_in_one.py
@@ -18,17 +18,19 @@ def kick_it_off(geojson):
     # check auth header or whatever
 
     unsigned_id, _ = upload_fields_new.generate_signed_id('no sig, no secret')
-    key = data.UPLOAD_PREFIX.format(id=unsigned_id) + 'plan.geojson'
+    upload_key = data.UPLOAD_PREFIX.format(id=unsigned_id) + 'plan.geojson'
+    index_key = data.UPLOAD_INDEX_KEY.format(id=unsigned_id)
+    index_url = constants.S3_URL_PATTERN.format(b=constants.S3_BUCKET, k=index_key)
 
     s3.put_object(
         Bucket=constants.S3_BUCKET,
-        Key=key,
+        Key=upload_key,
         Body=json.dumps(geojson, indent=2),
         ContentType='text/json',
         ACL='bucket-owner-full-control',
         )
 
-    upload1 = preread.create_upload(s3, constants.S3_BUCKET, key, unsigned_id)
+    upload1 = preread.create_upload(s3, constants.S3_BUCKET, upload_key, unsigned_id)
     storage = data.Storage(s3, constants.S3_BUCKET, None)
     observe.put_upload_index(storage, upload1)
     
@@ -59,9 +61,13 @@ def kick_it_off(geojson):
     # hand off to postread_calculate
     # return path of index JSON
     
+    return index_url
+    
     return {
         'unsigned_id': unsigned_id,
-        'key': key,
+        'upload_key': upload_key,
+        'index_key': index_key,
+        'index_url': index_url,
         'upload': upload3.to_dict(),
     }
 
@@ -69,7 +75,13 @@ def lambda_handler(event, context):
     '''
     '''
     geojson = json.loads(event['body'])
-    result = kick_it_off(geojson)
+    redirect_url = kick_it_off(geojson)
+    
+    return {
+        'statusCode': '302',
+        'headers': {'Location': redirect_url},
+        'body': ''
+        }
     
     return {
         'statusCode': '200',
