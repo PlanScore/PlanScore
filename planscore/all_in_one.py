c98cd5170531ad2ef0be1e2842826f7be3babf44
Michal Migurski <mike@teczno.com>
2021-03-06 10:56:23 -0800

    Extended API process to PlanScore-PostreadCalculate handoff

--- planscore/all_in_one.py
+++ planscore/all_in_one.py
@@ -7,11 +7,15 @@ from . import preread
 from . import data
 from . import observe
 from . import preread_followup
+from . import postread_calculate
 
-def kick_it_off(geojson_body):
+def kick_it_off(geojson):
     '''
     '''
     s3 = boto3.client('s3')
+    lam = boto3.client('lambda')
+    
+    # check auth header or whatever
 
     unsigned_id, _ = upload_fields_new.generate_signed_id('no sig, no secret')
     key = data.UPLOAD_PREFIX.format(id=unsigned_id) + 'plan.geojson'
@@ -19,27 +23,53 @@ def kick_it_off(geojson_body):
     s3.put_object(
         Bucket=constants.S3_BUCKET,
         Key=key,
-        Body=geojson_body,
+        Body=json.dumps(geojson, indent=2),
         ContentType='text/json',
         ACL='bucket-owner-full-control',
         )
 
-    upload = preread.create_upload(s3, constants.S3_BUCKET, key, unsigned_id)
+    upload1 = preread.create_upload(s3, constants.S3_BUCKET, key, unsigned_id)
     storage = data.Storage(s3, constants.S3_BUCKET, None)
-    observe.put_upload_index(storage, upload)
-    preread_followup.commence_upload_parsing(s3, constants.S3_BUCKET, upload)
+    observe.put_upload_index(storage, upload1)
+    
+    # First handoff should happen here
+    
+    upload2 = preread_followup.commence_upload_parsing(s3, constants.S3_BUCKET, upload1)
+    
+    upload3 = upload2.clone(
+        description = geojson.get('description', 'plan.geojson'),
+        incumbents = [
+            feature['properties'].get('Incumbent', 'O')
+            for feature in geojson['features']
+        ],
+    )
+
+    observe.put_upload_index(storage, upload3)
+    
+    event = dict(bucket=constants.S3_BUCKET)
+    event.update(upload3.to_dict())
+
+    lam.invoke(
+        FunctionName=postread_calculate.FUNCTION_NAME,
+        InvocationType='Event',
+        Payload=json.dumps(event).encode('utf8'),
+    )
+
+    # assign description and incumbents as in postread_callback.py
+    # hand off to postread_calculate
+    # return path of index JSON
     
     return {
         'unsigned_id': unsigned_id,
         'key': key,
-        'upload': upload.to_dict(),
+        'upload': upload3.to_dict(),
     }
 
 def lambda_handler(event, context):
     '''
     '''
-    geojson_body = event['body']
-    result = kick_it_off(geojson_body)
+    geojson = json.loads(event['body'])
+    result = kick_it_off(geojson)
     
     return {
         'statusCode': '200',
