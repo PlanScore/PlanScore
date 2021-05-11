44730dd420db0510d1739bd83adfd67b7648a4a2
Michal Migurski <mike@teczno.com>
2021-05-10 17:38:09 -0700

    Added CORS support for /states API response

--- planscore/get_states.py
+++ planscore/get_states.py
@@ -6,6 +6,7 @@ def lambda_handler(event, context):
     '''
     return {
         'statusCode': '200',
+        'headers': {'Access-Control-Allow-Origin': '*'},
         'body': json.dumps(
             [(m.state.value, m.house.value) for m in data.MODELS2020],
             indent=2,
