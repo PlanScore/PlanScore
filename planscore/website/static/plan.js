b36e082a6c2babf84168a6894b7c785853e7f158
Michal Migurski <mike@teczno.com>
2021-08-07 22:41:55 -0700

    Updated correct sentence for declination scores

--- planscore/website/static/plan.js
+++ planscore/website/static/plan.js
@@ -286,10 +286,34 @@ function show_efficiency_gap_score(plan, score_EG)
     }
 }
 
+function calculate_declination2_difference(plan)
+{
+    var dem_districts = [],
+        rep_districts = [];
+    
+    for(var i = 0; i < plan.districts.length; i++)
+    {
+        var dem_votes = plan.districts[i].totals['Democratic Votes'],
+            rep_votes = plan.districts[i].totals['Republican Votes'];
+        
+        if(dem_votes > rep_votes) {
+            dem_districts.push(dem_votes / (dem_votes + rep_votes));
+        } else {
+            rep_districts.push(rep_votes / (dem_votes + rep_votes));
+        }
+    }
+    
+    var dem_margin_avg = dem_districts.reduce((a, b) => (a + b), 0) / dem_districts.length,
+        rep_margin_avg = rep_districts.reduce((a, b) => (a + b), 0) / rep_districts.length;
+    
+    return rep_margin_avg - dem_margin_avg;
+}
+
 function show_declination2_score(plan, score_DEC2)
 {
     var declination = plan.summary['Declination'],
-        dec2_amount = Math.round(Math.abs(declination) * 100) / 100;
+        dec2_amount = Math.round(Math.abs(declination) * 100) / 100,
+        dec2_difference = calculate_declination2_difference(plan);
     
     for(node = score_DEC2.firstChild; node = node.nextSibling; node)
     {
@@ -301,22 +325,23 @@ function show_declination2_score(plan, score_DEC2)
                 (plan.model ? plan.model.house : 'ushouse'), 'plan');
 
         } else if(node.nodeName == 'P') {
-            var win_party = (declination < 0 ? 'Republican' : 'Democratic'),
-                win_partisans = (declination < 0 ? 'Republicans' : 'Democrats'),
-                lose_party = (declination < 0 ? 'Democratic' : 'Republican');
+            var win_partisans = (declination < 0 ? 'Republicans' : 'Democrats'),
+                lose_partisans = (declination < 0 ? 'Democrats' : 'Republicans');
 
             clear_element(node);
             
-            if(typeof plan.summary['Declination Positives'] === 'number') {
+            if(typeof plan.summary['Declination Positives'] === 'number')
+            {
                 var positives = (declination < 0
                     ? (1 - plan.summary['Declination Positives'])
                     : plan.summary['Declination Positives']);
             
                 node.innerHTML = [
-                    'Votes for tktktk', win_party, 'candidates are expected to be inefficient at a rate',
-                    dec2_amount, 'lower than votes for', lose_party, 'candidates.',
-                    'The expected gap favors', win_partisans,
-                    'in', nice_round_percent(positives), 'of predicted scenarios.',
+                    'The', lose_partisans+"’", 'mean vote share in districts they won was',
+                    nice_percent(Math.abs(dec2_difference)), 'higher than the', win_partisans+"’",
+                    'mean vote share in districts they won.',
+                    'This leads to a declination that favors Republicans in',
+                    nice_round_percent(positives), 'of predicted scenarios.',
                     '<a href="' + window.eg_metric_url + '">Learn more <i class="glyphicon glyphicon-chevron-right" style="font-size:0.8em;"></i></a>'
                     ].join(' ');
             }
@@ -1357,6 +1382,7 @@ if(typeof module !== 'undefined' && module.exports)
         update_acs2015_percentages,
         update_acs2016_percentages,
         update_cvap2015_percentages,
-        update_heading_titles
+        update_heading_titles,
+        calculate_declination2_difference,
     };
 }
