3a8b1067a228fd66604771717b5d3f0a692dad63
Michal Migurski <mike@teczno.com>
2025-08-28 20:57:04 -0700

    Updated CA to blocks file with 2024 pvote

--- planscore/data.py
+++ planscore/data.py
@@ -432,10 +432,10 @@ MODELS = [
     Model(State.AZ, House.statesenate,  30,  True, VERSIONS, 'data/AZ/012-acs-2020'), # c82db89
     Model(State.AZ, House.statehouse,   60,  True, VERSIONS, 'data/AZ/012-acs-2020'), # c82db89
     Model(State.AZ, House.localplan,  None,  True, VERSIONS, 'data/AZ/012-acs-2020'), # c82db89
-    Model(State.CA, House.ushouse,      52,  True, VERSIONS, 'data/CA/008-acs-2020'), # c82db89
-    Model(State.CA, House.statesenate,  40,  True, VERSIONS, 'data/CA/008-acs-2020'), # c82db89
-    Model(State.CA, House.statehouse,   80,  True, VERSIONS, 'data/CA/008-acs-2020'), # c82db89
-    Model(State.CA, House.localplan,  None,  True, VERSIONS, 'data/CA/008-acs-2020'), # c82db89
+    Model(State.CA, House.ushouse,      52,  True, VERSIONS, 'data/CA/009-pvote-2024'), # 41cd3c4
+    Model(State.CA, House.statesenate,  40,  True, VERSIONS, 'data/CA/009-pvote-2024'), # 41cd3c4
+    Model(State.CA, House.statehouse,   80,  True, VERSIONS, 'data/CA/009-pvote-2024'), # 41cd3c4
+    Model(State.CA, House.localplan,  None,  True, VERSIONS, 'data/CA/009-pvote-2024'), # 41cd3c4
     Model(State.CO, House.ushouse,       8,  True, VERSIONS, 'data/CO/013-acs-2020'), # c82db89
     Model(State.CO, House.statesenate,  35,  True, VERSIONS, 'data/CO/013-acs-2020'), # c82db89
     Model(State.CO, House.statehouse,   65,  True, VERSIONS, 'data/CO/013-acs-2020'), # c82db89
