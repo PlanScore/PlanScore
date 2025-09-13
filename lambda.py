7047d91de773bd6d37c7224d0b85da2606e27c5f
Michal Migurski <mike@teczno.com>
2025-09-13 14:32:38 -0700

    Reducing lambda.py to just the small functions

--- lambda.py
+++ lambda.py
@@ -1,12 +1,15 @@
+# Small zipfile functions
 from planscore.upload_fields import lambda_handler as upload_fields
-from planscore.preread import lambda_handler as preread
-from planscore.preread_followup import lambda_handler as preread_followup
-from planscore.postread_callback import lambda_handler_GET as postread_callback_GET
-from planscore.postread_callback import lambda_handler_POST as postread_callback_POST
-from planscore.postread_calculate import lambda_handler as postread_calculate
-from planscore.postread_intermediate import lambda_handler as postread_intermediate
 from planscore.authorizer import lambda_handler as authorizer
-from planscore.api_upload import lambda_handler as api_upload
 from planscore.get_states import lambda_handler as get_states
 from planscore.get_model_versions import lambda_handler as get_model_versions
-from planscore.polygonize import lambda_handler as polygonize
+
+# Large docker functions
+# from planscore.preread import lambda_handler as preread
+# from planscore.preread_followup import lambda_handler as preread_followup
+# from planscore.postread_callback import lambda_handler_GET as postread_callback_GET
+# from planscore.postread_callback import lambda_handler_POST as postread_callback_POST
+# from planscore.postread_calculate import lambda_handler as postread_calculate
+# from planscore.postread_intermediate import lambda_handler as postread_intermediate
+# from planscore.api_upload import lambda_handler as api_upload
+# from planscore.polygonize import lambda_handler as polygonize
